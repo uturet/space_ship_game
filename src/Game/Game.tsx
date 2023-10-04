873c6ea37974fc10f6b5a9756a9ab56c241928e3
Sergey Tsybenko <cybenko.sergey@gmail.com>
2023-10-04 10:40:03 -0600

    Draw a ship in the Game field

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -9,40 +9,124 @@ function Game (): JSX.Element {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setX((v) => v + 1)
-      setY((v) => v + 1)
+      setX((v) => v)
+      setY((v) => v)
     }, 16)
     return () => { clearInterval(interval) }
   }, [x, y])
 
   useEffect(() => {
     if (ctx !== null && ctx !== undefined) {
-      const a = 2 * Math.PI / 6
-      const r = 50
       ctx.clearRect(0, 0, width, height)
-
+      // const blueColor = '#283891'
+      const blueColor = '#983891'
+      const grayColor = '#58585A'
 
       ctx.beginPath()
-      for (let i = 0; i < 6; i++) {
-        ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i))
-      }
+      ctx.lineWidth = 0
+      ctx.strokeStyle = blueColor
+      ctx.fillStyle = blueColor
+      const midw = 655
+      const midh = height / 2
+      const shift = 170
+
+      ctx.ellipse(midw, (midh) + 75, 210, 70, 0, Math.PI * 7 / 8, Math.PI * 34 / 16)
+      ctx.fill()
+
+      // Wing
+      ctx.beginPath()
+      ctx.moveTo(midw - shift, 494)
+      ctx.lineTo(midw, 430)
+      ctx.lineTo(midw + shift, 494)
+      ctx.closePath()
+      ctx.fillStyle = blueColor
+      ctx.fill()
+
+      // Nose
+      ctx.beginPath()
+      ctx.moveTo(midw - 55, 425)
+      ctx.lineTo(midw - 55, 400)
+      ctx.lineTo(midw - 10, 335)
+      ctx.lineTo(midw + 10, 335)
+      ctx.lineTo(midw + 55, 400)
+      ctx.lineTo(midw + 55, 425)
       ctx.closePath()
       ctx.stroke()
+      ctx.fillStyle = blueColor
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.moveTo(midw - 55, 425)
+      ctx.lineTo(midw - 50, 430)
+      ctx.lineTo(midw - 50, 470)
+      ctx.lineTo(midw + 50, 470)
+      ctx.lineTo(midw + 50, 430)
+      ctx.lineTo(midw + 55, 425)
+      ctx.closePath()
+
+      ctx.fillStyle = grayColor
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.moveTo(midw - 50, 580)
+      ctx.lineTo(midw - 50, 520)
+      ctx.lineTo(midw - 40, 520)
+      ctx.lineTo(midw - 35, 510)
+      ctx.lineTo(midw - 10, 500)
+      ctx.lineTo(midw + 10, 500)
+      ctx.lineTo(midw + 35, 510)
+      ctx.lineTo(midw + 40, 520)
+      ctx.lineTo(midw + 50, 520)
+      ctx.lineTo(midw + 50, 580)
+      ctx.closePath()
+
+      ctx.fillStyle = grayColor
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.moveTo(midw - 50, 580)
+      ctx.lineTo(midw - 50, 520)
+      ctx.lineTo(midw - 40, 520)
+      ctx.lineTo(midw - 35, 510)
+      ctx.lineTo(midw - 10, 500)
+      ctx.lineTo(midw + 10, 500)
+      ctx.lineTo(midw + 35, 510)
+      ctx.lineTo(midw + 40, 520)
+      ctx.lineTo(midw + 50, 520)
+      ctx.lineTo(midw + 50, 580)
+      ctx.closePath()
+
+      ctx.fillStyle = grayColor
+
+      ctx.fill()
+      ctx.translate(midw, midh)
+      ctx.rotate(Math.PI / 2)
+      ctx.translate(-midw, -midh)
     }
   }, [ctx, x, y])
 
-  return <canvas
-    ref={(canvas) => {
-      const context = canvas?.getContext('2d')
-      if (context !== null && context !== undefined) {
-        setCtx(context)
-      }
-    }}
-    width={width}
-    height={height}
-    className="w-screen h-screen">
+  return <div>
+    <img style={{
+      width: '500px',
+      position: 'absolute',
+      left: '405px',
+      top: '209px',
+      opacity: 0.5
 
-  </canvas>
+    }} src="./space-ship.png" alt="" />
+    <canvas
+      ref={(canvas) => {
+        const context = canvas?.getContext('2d')
+        if (context !== null && context !== undefined) {
+          setCtx(context)
+        }
+      }}
+      width={width}
+      height={height}
+      className="w-screen h-screen">
+
+    </canvas>
+  </div>
 }
 
 export default Game
